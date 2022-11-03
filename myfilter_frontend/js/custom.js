eb115a783797651be4a2600911a0eee64288cbe7
Mingyu Li <lmytime@hotmail.com>
2022-11-03 23:28:04 +0800

    first commit

---
+++ myfilter_frontend/js/custom.js
@@ -0,0 +1,170 @@
+import allLines from "../lines/llist_v1.3.json" assert {
+    type: "json"
+};
+import FilterIndexing from "../filters/indexing.json" assert {
+    type: "json"
+};
+
+const app = Vue.createApp({
+    data() {
+        return {
+            g: "",
+            redshift: 0,
+            lines: "",
+            FilterIndexing: FilterIndexing,
+            selectedIntrument:[],
+            filter: "",
+            dialogFormVisible: false,
+            form: {
+                name: "",
+                lambda: "",
+                checked: true
+            },
+            loading: false,
+            linList: allLines,
+            lineOptions: [],
+            instrumentOptions: [],
+            value: "",
+            InstrumentValue: ""
+        }
+    },
+    methods: {
+        //
+        // Helper function used by underlaycallback.
+        //
+        getLeftRight(centerWave, g) {
+            let center = g.toDomCoords(centerWave, 0);
+            return [center[0] - 1.5, center[0] + 1.5];
+        },
+        //
+        // This function draws the spectra line markers.
+        //
+        underlaycallback(canvas, area, g) {
+            g.redshift < -0.1 ? g.redshift = -0.1 : this.redshift = g.redshift
+            for (let i = 0; i < this.lines.length; i++) {
+                if (this.lines[i].checked === false) continue;
+                let shiftedWave = this.lines[i].lambda * (1.0 + g.redshift);
+                let leftright = this.getLeftRight(shiftedWave, g);
+                canvas.fillStyle = "rgba(0, 38, 220, 0.6)"
+                canvas.fillRect(leftright[0], area.y, leftright[1] - leftright[0], area.h);
+                canvas.font = "15px serif";
+                canvas.fillStyle = "rgba(0, 38, 220, 1)"
+                let offset = (i % 3) * 15 + 32;
+                let texty = area.y + offset;
+                // let texty = lines[i].emission ? area.y + offset : area.y + area.h - offset;
+                canvas.fillText(this.lines[i].name, leftright[0], texty);
+            }
+        },
+        updateline() {
+            this.g.redshift = this.redshift
+            this.g.updateOptions({});
+        },
+        addLine() {
+            this.lines.push(this.form);
+            this.form = {
+                name: "",
+                lambda: "",
+                checked: true
+            };
+            this.g.updateOptions({});
+        },
+        updateFilter() {
+            this.filter = []
+            for (let instrument of this.selectedIntrument) {
+                for (let ff of instrument.children) {
+                    if (ff.checked) {
+                        this.filter.push(ff.value)
+                    }
+                }
+            }
+            if (this.filter.length === 0) {
+                this.filter = ["CFHT/MegaCam.u.dat", "Subaru/HSC.g.dat", "Subaru/HSC.r.dat", "Subaru/HSC.i.dat", "Subaru/HSC.z.dat", "Subaru/HSC.Y.dat"]
+            }
+            this.g.updateOptions({
+                'file': `https://preview.lmytime.com/getfilter?${this.filter.join('&')}`
+            });
+        },
+        searchLine(query) {
+            if (query) {
+                this.loading = true
+                this.lineOptions = this.linList.filter((item) => {
+                    return (item.name+"|"+item.w).toLowerCase().includes(query.replace(/\s/g, '').toLowerCase())
+                })
+                this.loading = false
+            } else {
+                this.lineOptions = []
+            }
+        },
+        addSearchedLine(value) {
+            this.lines.unshift({
+                "name": value.name,
+                "lambda": value.w,
+                "checked": true
+            })
+            this.g.updateOptions({});
+            this.value = ""
+        },
+        searchInstrument(query) {
+            if (query) {
+                this.loading = true
+                this.instrumentOptions = this.FilterIndexing.filter((item) => {
+                    return (item.value).toLowerCase().includes(query.replace(/\s/g, '').toLowerCase())
+                })
+                this.loading = false
+            } else {
+                this.instrumentOptions = []
+            }
+        },
+        addSearchedInstrument(value) {
+            this.selectedIntrument.push(value)
+            this.InstrumentValue = ""
+        }
+    },
+    beforeMount() {
+        const params = new URLSearchParams(window.location.search);
+        const paramsObj = Array.from(params.keys()).reduce(
+            (acc, val) => ({ ...acc, [val]: params.get(val) }),
+            {}
+        );
+        axios.get("/myfilter/lines/default.json").then(res => {
+            this.lines = res.data;
+        }).catch(err => {
+            console.log(err);
+        }).finally(() => {
+            this.updateFilter();
+        }
+        )
+    },
+    mounted() {
+        this.g = new Dygraph(document.getElementById("dygraph"),
+            `https://preview.lmytime.com/getfilter?CFHT/MegaCam.u.dat&Subaru/HSC.g.dat&Subaru/HSC.r.dat&Subaru/HSC.i.dat&Subaru/HSC.z.dat&Subaru/HSC.Y.dat`, {
+                title: 'My Filter',
+                xlabel: 'Wavelength [Å]',
+                ylabel: 'Response',
+                legend: 'follow',
+                fillGraph: true,
+                rollPeriod: 1,
+                animatedZooms: true,
+                interactionModel: Dygraph.MyInteractionModel,
+                underlayCallback: this.underlaycallback,
+            });
+
+        // initialize using Subaru/HSC g, r, i, z, Y filters and CFHT/MegaCam u filter
+        this.selectedIntrument = [this.FilterIndexing[393], this.FilterIndexing[159]]
+        this.selectedIntrument[0]['children'][3]['checked'] = true
+        this.selectedIntrument[0]['children'][5]['checked'] = true
+        this.selectedIntrument[0]['children'][7]['checked'] = true
+        this.selectedIntrument[0]['children'][16]['checked'] = true
+        this.selectedIntrument[0]['children'][21]['checked'] = true
+        this.selectedIntrument[1]['children'][10]['checked'] = true
+
+        this.updateFilter()
+        this.g.redshift = 2.3
+    }
+})
+
+app.use(ElementPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+app.mount('#app')
---
+++ run.py
@@ -0,0 +1,53 @@
+from scipy import interpolate
+import pandas as pd
+import numpy as np
+from flask import Flask, request, redirect, send_file, render_template, send_from_directory
+import requests, io
+from flask_cors import CORS
+app = Flask(__name__, static_url_path='/myfilter', template_folder='./myfilter_frontend', static_folder='./myfilter_frontend')
+CORS(app)
+
+
+def combine_filter(urls):
+    filter = {band: pd.read_csv(urls[band], header=None, delim_whitespace=True, names=['w', 't'], comment='#').apply(
+        pd.to_numeric, errors='ignore') for band in urls}
+
+    filter_interpolate = {}
+    _wavelength = []
+    for filname in filter:
+        w = filter[filname]['w']
+        _wavelength = _wavelength + list(w)
+        t = filter[filname]['t']
+        f = interpolate.interp1d(w, t, kind='slinear',
+                                 bounds_error=False, fill_value=0)
+        filter_interpolate[filname] = f
+    wmin, wmax = min(_wavelength), max(_wavelength)
+    step = 5
+    final_t = {'w': np.arange(wmin, wmax+step, step=step)}
+    for filname in filter_interpolate:
+        fil_interpolate = filter_interpolate[filname]
+        final_t[filname] = fil_interpolate(final_t['w'])
+    final = pd.DataFrame(final_t)
+    return final.to_csv(index=None)
+
+
+@app.route('/getfilter', methods=['GET'])
+def get_filter():
+    urls = {fil[:-4]: "./filters/"+fil for fil in request.args}
+
+    file = combine_filter(urls)
+    proxy = io.StringIO(file)
+    mem = io.BytesIO()
+    mem.write(proxy.getvalue().encode())
+    mem.seek(0)
+    proxy.close()
+    return send_file(mem, mimetype='text/csv')
+
+@app.route('/myfilter', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=3688)
+    # app.run(debug=True, port=9899)
