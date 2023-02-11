fb815cf90076c319ad7f62036d338385cfe34223
Mingyu Li <lmytime@hotmail.com>
2023-02-11 17:10:36 +0800

    add link share; better download;

--- myfilter_frontend/js/custom.js
+++ myfilter_frontend/js/custom.js
@@ -13,9 +13,11 @@ const app = Vue.createApp({
             lines: "",
             FilterIndexing: FilterIndexing,
             selectedIntrument:[],
-            filter: "",
+            filter: [],
             filterInit: [],
+            filterInitDefault: ["CFHT/MegaCam.u", "Subaru/HSC.g", "Subaru/HSC.r", "Subaru/HSC.i", "Subaru/HSC.z", "Subaru/HSC.Y"],
             zInit: 2,
+            zInitDefault: 2,
             dialogFormVisible: false,
             form: {
                 name: "",
@@ -30,6 +32,7 @@ const app = Vue.createApp({
             value: "",
             InstrumentValue: "",
             filterLink: "",
+            shareLinkVisible: false
         }
     },
     methods: {
@@ -144,17 +147,56 @@ const app = Vue.createApp({
             // delete instrument
             this.selectedIntrument.splice(this.selectedIntrument.indexOf(instrument), 1)
             instrument.dialog = true
-        }
+        },
+        forceFileDownload(response, title) {
+            console.log(title)
+            const url = window.URL.createObjectURL(new Blob([response.data]))
+            const link = document.createElement('a')
+            link.href = url
+            link.setAttribute('download', title)
+            document.body.appendChild(link)
+            link.click()
+        },
+        downloadWithAxios(url, title) {
+            axios({
+                method: 'get',
+                url,
+                responseType: 'arraybuffer',
+            }).then((response) => {
+                this.forceFileDownload(response, title)
+            }).catch(() => console.log('error occured'))
+        },
     },
     beforeMount() {
+        console.log("Welcome to MyFilter!");
+        console.log("If you have any ideas or suggestions, please contact Mingyu at lmytime@hotmail.com");
+        console.log("If you like this app, please share it with your friends!");
+        console.log("TODO: [1] better line systems; [2] better performance; [3] custom filter; [4] better everything.")
+
         const params = new URLSearchParams(window.location.search);
         const paramsObj = Array.from(params.keys()).reduce(
             (acc, val) => ({ ...acc, [val]: params.get(val) }),
             {}
         );
-        this.filterInit = paramsObj.filter ? paramsObj.filter.split(',') : ["CFHT/MegaCam.u", "Subaru/HSC.g", "Subaru/HSC.r", "Subaru/HSC.i", "Subaru/HSC.z", "Subaru/HSC.Y"]
-        this.zInit = paramsObj.z ? parseFloat(paramsObj.z) : 2
-        console.log(this.filterInit, this.zInit)
+        this.filterInit = paramsObj.fil ? paramsObj.fil.split(',') : this.filterInitDefault
+        this.zInit = paramsObj.z ? parseFloat(paramsObj.z) : this.zInitDefault
+
+        const CustomInitNotification = () => {
+            ElementPlus.ElNotification({
+              title: 'Custom Initialization Success',
+              dangerouslyUseHTMLString: true,
+              message: `<strong><i>MyFilter</i> app is successfully initialized with the custom-selected filters and redshift.
+              You can share the link with others to show the same filter set and redshift.</strong>`,
+              type: 'success',
+            })
+        }
+
+        if((this.filterInit == this.filterInitDefault) && (this.zInit == this.zInitDefault)){
+            this.shareLinkVisible = false
+        } else{
+            this.shareLinkVisible = true
+            CustomInitNotification()
+        }
         axios.get("/myfilter/lines/default.json").then(res => {
             this.lines = res.data;
         }).catch(err => {
@@ -224,6 +266,11 @@ const app = Vue.createApp({
         this.selectedIntrument = [...this.instrumentInit]
         this.updateFilter()
         this.g.redshift = this.zInit
+    },
+    computed: {
+        shareLink: function () {
+            return `https://preview.lmytime.com/myfilter?fil=${this.filter.join(',')}&z=${this.redshift}`
+        }
     }
 })
 
